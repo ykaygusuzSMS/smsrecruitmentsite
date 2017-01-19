ee51262e3793ea2a61034d232c14d5cd793f6759
Mina Galdas <minagaldas@Minas-MacBook-Pro.local>
2017-01-19 11:45:51 +1100

    removed echo

--- index.php
+++ index.php
@@ -3,27 +3,11 @@ $servername = "localhost";
 $username = "root";
 $password = "deal97-manic";
 $dbname = "recruitment";
-
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die($conn->connect_error);
 }
-echo "Connected successfully <br>";
-
-$sql = "SELECT * FROM questions";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " - competency: " . $row["competency"]. " - subcategory:  " . $row["subcategory"]. " - question:  ". $row["question"]. " <br>";
-    }
-} else {
-    echo "0 results";
-}
-
 $conn->close();
 ?>
